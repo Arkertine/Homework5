25e214413c81c8949ad3d44df02255e0ad235322
Владислав <robert_trebor@list.ru>
2024-05-21 16:53:49 +0300

    Homework corrected

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Task 1");
-        int clientIOS = 1;
+        int clientOS = 1;
 
-        if (clientIOS == 0) {
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else if (clientIOS == 1) {
+        } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         } else {
             System.out.println("Несовместимость клиента.");
         }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         System.out.println("Task 2");
-        int releaseYear = 2020;
-        int clientAndroid = 1;
+        int releaseYear = 2014;
         int clientDeviceYear = 2015;
 
-        if (clientAndroid == 0 && releaseYear < clientDeviceYear) {
+        if (clientOS == 0 && releaseYear < clientDeviceYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-        } else if (clientAndroid == 0 && releaseYear >= clientDeviceYear) {
+        } else if (clientOS == 0 && releaseYear >= clientDeviceYear) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else if (clientAndroid == 1 && releaseYear < clientDeviceYear) {
+        } else if (clientOS == 1 && releaseYear < clientDeviceYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-        } else if (clientAndroid == 1 && releaseYear >= clientDeviceYear) {
+        } else if (clientOS == 1 && releaseYear >= clientDeviceYear) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -39,13 +38,13 @@ public class Main {
         }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         System.out.println("Task 4");
-        int deliveryDistance = 99;
+        int deliveryDistance = 95;
         int deliveryDays = 1;
 
         if (deliveryDistance < 20) {
             System.out.println("Потребуется дней: " + deliveryDays + ".");
         } else if (deliveryDistance < 60) {
-            deliveryDays ++;
+            deliveryDays++;
             System.out.println("Потребуется дней: " + deliveryDays + ".");
         } else if (deliveryDistance < 100) {
             deliveryDays += 2;
@@ -55,43 +54,43 @@ public class Main {
         }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         System.out.println("Task 5");
-        int monthNumber = 4;
+        int monthNumber = 12;
         switch (monthNumber) {
             case 12:
-                System.out.printf("%s - й месяц (он же декабрь) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же декабрь) принадлежит к сезону зима.%n", monthNumber);
                 break;
             case 1:
-                System.out.printf("%s - й месяц (он же январь) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же январь) принадлежит к сезону зима.%n", monthNumber);
                 break;
             case 2:
-                System.out.printf("%s - й месяц (он же февраль) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же февраль) принадлежит к сезону зима.%n", monthNumber);
                 break;
             case 3:
-                System.out.printf("%s - й месяц (он же март) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же март) принадлежит к сезону весна.%n", monthNumber);
                 break;
             case 4:
-                System.out.printf("%s - й месяц (он же апрель) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же апрель) принадлежит к сезону весна.%n", monthNumber);
                 break;
             case 5:
-                System.out.printf("%s - й месяц (он же май) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же май) принадлежит к сезону весна.%n", monthNumber);
                 break;
             case 6:
-                System.out.printf("%s - й месяц (он же июнь) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же июнь) принадлежит к сезону лето.%n", monthNumber);
                 break;
             case 7:
-                System.out.printf("%s - й месяц (он же июль) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же июль) принадлежит к сезону лето.%n", monthNumber);
                 break;
             case 8:
-                System.out.printf("%s - й месяц (он же август) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же август) принадлежит к сезону лето.%n", monthNumber);
                 break;
             case 9:
-                System.out.printf("%s - й месяц (он же сентябрь) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же сентябрь) принадлежит к сезону осень.%n", monthNumber);
                 break;
             case 10:
-                System.out.printf("%s - й месяц (он же октябрь) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же октябрь) принадлежит к сезону осень.%n", monthNumber);
                 break;
             case 11:
-                System.out.printf("%s - й месяц (он же ноябрь) принадлежит к сезону зима.%n" , monthNumber);
+                System.out.printf("%s - й месяц (он же ноябрь) принадлежит к сезону осень.%n", monthNumber);
                 break;
             default:
                 System.out.println("Номер месяца больше 12-ти.");
